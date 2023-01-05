7e14e66997e960ac2d80a29f2544bdbe534ec56c
Xylon Funes <xyfullns@gmail.com>
2023-01-04 22:38:56 -0500

    to

--- src/components/Todoform.js
+++ src/components/Todoform.js
@@ -1,16 +1,33 @@
 import React,{ useState } from 'react'
 
-function Todoform() {
+function Todoform(props) {
   const [input, setInput] = useState('');
+
+  console.log(props);
+  const handleChange = e => {
+    setInput(e.target.value);
+  }
+  const handlSub = e => {
+    e.preventDefault();
+
+    props.onSubmit({
+      id: Math.floor(Math.random()* 1000),
+      text: input
+    });
+
+    setInput('');
+  };
+
   return (
     
-     <form classname= 'todo-form'>
+     <form classname= 'todo-form' onSubmit={handlSub}>
       <input
       type='text'
       placeholder='Add a todo'
       value={input}
       name='text'
       className='todo-input'
+      onChange={handleChange}
       />
       <button className='todo-button'>Add todo</button>
      </form>
