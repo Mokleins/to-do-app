5073895d0aa4cac0459e606b0aa4068cc27e6d85
Xylon <xyfullns@gmail.com>
2023-01-27 13:48:55 -0500

    todo

--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -1,26 +1,53 @@
-import React, {useState} from 'react'
-import Todoform from './Todoform'
+import React, { useState } from "react";
+import Todoform from "./Todoform";
+import Todo from "./Todo";
 
 function Todolist() {
-    const[todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([]);
 
-    const addTodo = todo => {
-        if(!todo.text || /^\s*$/.test(todo.text)){
-            return;
-        }
-    
+  const addTodo = (todo) => {
+    // Line 9 is when the user decides to pun only blank, but it will not add
+    // to the list.
+    if (!todo.text || /^\s*$/.test(todo.text)) {
+      return;
+    }
+
     const newTodos = [todo, ...todos];
     setTodos(newTodos);
-    console.log(todo, ...todos)
+  };
+
+  const updateTodo = (todoId, newValue) => {
+    if (!newValue.text || /^\s*$/.test(newValue.text)) {
+      return;
     }
+
+    setTodos((prev) =>
+      prev.map((item) => (item.id === todoId ? newValue : item))
+    );
+  };
+
+  const removeTodo = (id) => {
+    const removeArr = [...todos].filter((todo) => todo.id !== id);
+    setTodos(removeArr);
+  };
+
+  const completeTodo = (id) => {
+    let updateTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(updateTodos);
+  };
 
   return (
     <div>
-        <h1>Todolist</h1>
-        <Todoform onSubmit={addTodo}/>
+      <h1>Todolist</h1>
+      <Todoform onSubmit={addTodo} />
+      <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} />
     </div>
-
-  )
+  );
 }
 
-export default Todolist
+export default Todolist;
